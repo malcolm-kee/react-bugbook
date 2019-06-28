6dd7f01dd1b8c735b37360a9fea769bcdaef0dff
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-06-28 22:30:49 +0800

    map data to elements

--- src/index.js
+++ src/index.js
@@ -2,68 +2,78 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
 
+const posts = [
+  {
+    id: 1111,
+    author: {
+      src: 'https://twitter.com/dog_feelings/status/1130660155966730240',
+      name: 'Thoughts of Dog 🏳️‍🌈'
+    },
+    post: {
+      text: 'to be perfectly clear. i love you',
+      image: null
+    }
+  },
+  {
+    id: 1,
+    author: {
+      src: 'https://twitter.com/markdalgleish/status/1095424850565378048',
+      name: 'Mark Dalgleish'
+    },
+    post: {
+      text:
+        "If you're not mocking out Lodash, can you really call it a unit test?",
+      image: null
+    }
+  },
+  {
+    id: 2,
+    author: {
+      src: 'https://twitter.com/iamdevloper/status/1081923027644882944',
+      name: 'I Am Devloper'
+    },
+    post: {
+      text:
+        'when you visit a site you handed over 6 months ago and the client made some changes themselves',
+      image: {
+        src: 'https://bugbook.netlify.com/images/picture-after-client.jpg',
+        alt: 'Ugly photo'
+      }
+    }
+  }
+];
+
 const feed = (
   <div className="feed">
-    <article className="card post">
-      <div className="card-title">
-        <a href="https://twitter.com/dog_feelings/status/1130660155966730240">
-          Thoughts of Dog 🏳️‍🌈
-        </a>
-        <button className="focus-btn" type="button">
-          ...
-        </button>
-      </div>
-      <div className="card-content">to be perfectly clear. i love you</div>
-      <div className="card-actions">
-        <button type="button" className="button">
-          Like
-        </button>
-      </div>
-    </article>
-    <article className="card post">
-      <div className="card-title">
-        <a href="https://twitter.com/markdalgleish/status/1095424850565378048">
-          Mark Dalgleish
-        </a>
-        <button className="focus-btn" type="button">
-          ...
-        </button>
-      </div>
-      <div className="card-content">
-        If you're not mocking out Lodash, can you really call it a unit test?
-      </div>
-      <div className="card-actions">
-        <button type="button" className="button">
-          Like
-        </button>
-      </div>
-    </article>
-    <article className="card post">
-      <div className="card-title">
-        <a href="https://twitter.com/iamdevloper/status/1081923027644882944">
-          I Am Devloper
-        </a>
-        <button className="focus-btn" type="button">
-          ...
-        </button>
-      </div>
-      <div className="card-content">
-        when you visit a site you handed over 6 months ago and the client made
-        some changes themselves
-      </div>
-      <div className="card-image-container">
-        <img
-          className="card-image"
-          src="https://bugbook.netlify.com/images/picture-after-client.jpg"
-          alt="Ugly"
-        />
-      </div>
-      <div className="card-actions">
-        <button type="button" className="button">
-          Like
-        </button>
-      </div>
-    </article>
+    {posts.map(post => {
+      const img = post.post.image ? (
+        <div className="card-image-container">
+          <img
+            className="card-image"
+            src={post.post.image.src}
+            alt={post.post.image.alt}
+          />
+        </div>
+      ) : null;
+
+      return (
+        <article className="card post" key={post.id}>
+          <div className="card-title">
+            <a href={post.author.src}>{post.author.name}</a>
+            <button className="focus-btn" type="button">
+              ...
+            </button>
+          </div>
+          <div className="card-content">{post.post.text}</div>
+          {img}
+          <div className="card-actions">
+            <button type="button" className="button">
+              Like
+            </button>
+          </div>
+        </article>
+      );
+    })}
   </div>
 );
 
